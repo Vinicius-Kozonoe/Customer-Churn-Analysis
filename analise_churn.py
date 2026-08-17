edd482469b1ff71f001f2eee784fd91eb5a6025d
Vinicius <kozonoe.vinicius@gmail.com>
2026-08-17 20:20:19 -0300

    feat:preenche mensalidade nula com mediana por plano e cria faixa etaria e faixa de tempo de contrato

--- analise_churn.py
+++ analise_churn.py
@@ -8,3 +8,14 @@ df.duplicated().sum()
 
 df = df.drop_duplicates()
 df['Plano'] = df['Plano'].fillna('Nao informado')
+
+df['Mensalidade'] = df.groupby('Plano')['Mensalidade'].transform(lambda x: x.fillna(x.median()))
+
+print(df.isnull().sum())
+
+bins = [0, 25, 35, 45, 55, 100]
+labels = ['18-25', '26-35', '36-45', '46-55', '56+']
+df['Faixa_Etaria'] = pd.cut(df['Idade'], bins=bins, labels=labels)
+
+df['Faixa_Contrato'] = pd.cut(df['Tempo_Contrato_Meses'], bins=[0, 12, 24, 36, 48, 100],
+                                labels=['0-12', '13-24', '25-36', '37-48', '49+'])git 
